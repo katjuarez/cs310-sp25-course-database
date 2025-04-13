fbee2dfdd50e7e184643d3a198d3a20ab83b30c2
katjuarez <kattyjuarez5@gmail.com>
2025-04-13 17:11:52 -0500

    fix: attempting to fix

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -17,6 +17,23 @@ public class DAOUtility {
             if (rs != null) {
 
                 // INSERT YOUR CODE HERE
+                
+                ResultSetMetaData meta = rs.getMetaData();
+                int columnCount = meta.getColumnCount();
+
+                while (rs.next()) {
+                    JsonObject obj = new JsonObject();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = meta.getColumnName(i);
+                        
+                        Object value = rs.getObject(i);
+                        String stringValue = (value != null) ? value.toString() : "";
+                        
+                        obj.put(columnName, stringValue);
+
+                    }
+                    records.add(obj);
+                }
 
             }
             
--- src/edu/jsu/mcis/cs310/coursedb/dao/RegistrationDAO.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/RegistrationDAO.java
@@ -29,6 +29,15 @@ public class RegistrationDAO {
                 
                 // INSERT YOUR CODE HERE
                 
+                String query = "INSERT INTO registration (studentid, termid, crn) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE crn = VALUES(crn)";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+
+                int rowsAffected = ps.executeUpdate();
+                result = (rowsAffected != 0);
+                
             }
             
         }
@@ -60,6 +69,15 @@ public class RegistrationDAO {
                 
                 // INSERT YOUR CODE HERE
                 
+                String query = "DELETE FROM registration WHERE studentid = ? AND termid = ? AND crn = ?";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                ps.setInt(3, crn);
+
+                int rowsAffected = ps.executeUpdate();
+                result = (rowsAffected != 0);
+                
             }
             
         }
@@ -89,6 +107,14 @@ public class RegistrationDAO {
             if (conn.isValid(0)) {
                 
                 // INSERT YOUR CODE HERE
+                
+                String query = "DELETE FROM registration WHERE studentid = ? AND termid = ?";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+
+                int rowsAffected = ps.executeUpdate();
+                result = (rowsAffected != 0);
                 
             }
             
@@ -121,6 +147,14 @@ public class RegistrationDAO {
             if (conn.isValid(0)) {
                 
                 // INSERT YOUR CODE HERE
+                
+                String query = "SELECT * FROM registration WHERE studentid = ? AND termid = ? ORDER BY crn";
+                ps = conn.prepareStatement(query);
+                ps.setInt(1, studentid);
+                ps.setInt(2, termid);
+                rs = ps.executeQuery();
+
+                result = DAOUtility.getResultSetAsJson(rs);
                 
             }
             
